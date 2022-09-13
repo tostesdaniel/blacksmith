9f4f256a6649df8d77a5547e3d8c3b6f250e14ab
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-13 16:41:39 -0300

    feat: add product router into app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', routes.productsRouter);
 
 export default app;
