af1cd3620c2164ab9eb2e7cdbc92d04f6405ab89
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-14 23:31:54 -0300

    feat: create User interfaces

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,7 +1,19 @@
+import { Request } from 'express';
+import { RowDataPacket } from 'mysql2/promise';
+
 export default interface User {
   id: number;
   username: string;
   classe: string;
   level: number;
   password: string;
+}
+
+export interface UserPacket extends User, RowDataPacket {}
+
+export interface UserRequest extends Request {
+  user?: {
+    id: number;
+    username: string;
+  };
 }
