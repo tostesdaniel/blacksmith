9e27dff741b54981d7b3ff0015d90371562a082c
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-14 23:39:05 -0300

    feat: add login routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import errorMiddleware from './middlewares/error.middleware';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', routes.loginRouter);
 app.use('/orders', routes.orderRouter);
 app.use('/products', routes.productsRouter);
 app.use('/users', routes.userRouter);
+
+app.use(errorMiddleware);
 
 export default app;
